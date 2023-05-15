0d7e2deab802cf2c1c7f38adf71fe92964eaca87
luc siat <lucsiat@gmail.com>
2023-05-15 13:10:56 +0200

    uses .env for the api url

--- frontend/src/services/mockeaApi.ts
+++ frontend/src/services/mockeaApi.ts
@@ -4,9 +4,9 @@ import { IProduct } from "../interfaces";
 // use import.meta.env.VITE_API_SERVER_URL when you can
 
 // const LocalProductUrl = "http://localhost:5046/api/Products/";
-const ProductUrl = "https://mockea-backend.azurewebsites.net/api/Products";
+// const ProductUrl = "https://mockea-backend.azurewebsites.net/api/Products";
 
 export const getProducts = async () => {
-  const response =  await fetch(ProductUrl);
+  const response =  await fetch(`${import.meta.env.VITE_API_SERVER_URL}/api/Products`);
   return await response.json() as IProduct[];
 }
