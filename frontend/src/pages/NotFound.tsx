b4728cfac0029d9d47493cbbfbd536b118c2dfd0
Atte Pitkänen <118916517+attepitkaenen@users.noreply.github.com>
2023-05-17 14:36:04 +0200

    Update NotFound.tsx

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -2,7 +2,7 @@
 export const NotFound = () => {
   return (
     <body className="flex justify-center flex-wrap">
-      <img src="src/assets/notfound.png" alt="mockeaLogo" className=" w-96 h-96 py-2.5 mt-20" />
+      <img src="src/assets/notfound.png" alt="page not found" className=" w-96 h-96 py-2.5 mt-20" />
       <p className="text-center w-full">404: The page you were looking for was not found.</p>
     </body>
   )
