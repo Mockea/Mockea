4cd13a97bc339ccdb35cd78602eb8677ecc34668
luc siat <lucsiat@gmail.com>
2023-05-17 10:58:56 +0200

    add links to footer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
