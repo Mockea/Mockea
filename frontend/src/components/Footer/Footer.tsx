2d1fe69803df74b06535dcb31dac450316427e12
luc siat <lucsiat@gmail.com>
2023-05-25 15:21:17 +0200

    refacto after marcus input

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom"
 import { FooterLinks } from "./FooterLinks"
+import { FooterAffiliatedLink } from "./FooterAffiliatedLink"
+import { FooterSupportedPayments } from "./FooterSupportedPayments"
 
 export const Footer = () => {
 
@@ -51,26 +54,23 @@ export const Footer = () => {
       ["Press room" , ""]
     ]
 
+    const footerSocials = [
+      ["facebook", ""],
+      ["instagram", ""],
+      ["pinterest", ""],
+      ["youtube", ""],
+      ["twitter", ""],
+    ]
 
-  // family.map(element =>  <li><a href={value}>{key}</a></li>)
-
+    const FooterSocialsJSX = footerSocials.map(social =>  <li><Link to=""><i className={`fa-brands fa-${social} footer__links-icons`}></i></Link></li>)
 
   return (
     <div className="footer relative bottom-0 bg-grey py-10 mt-20 pt-20">
       <div className="container mx-auto text-center flex flex-col gap-5">
         <div className="footer-content grid grid-cols-[2fr_1fr_1fr_1fr_1fr]">
             <ul className="footer__featured-links pe-20 text-start me-16">
-                <li className="px-5 pb-5 flex flex-col gap-5">
-                    <h3 className="font-bold">Join the MOCKEA Family</h3>
-                    <p className="text-sm">Give your ideas new life with offers,
-                     inspiration and lots of activities in our department stores - completely free of charge.</p>
-                    <button className="featured-links__button">Become a member here</button>
-                </li>
-                <li className="px-5 pb-5 flex flex-col gap-5">
-                    <h3 className="font-bold">MOCKEA for business</h3>
-                    <p className="text-sm">Our company experts help with flexible, affordable and more sustainable solutions.</p>
-                    <button className="featured-links__button">MOCKEA for business</button>
-                </li>
+                <FooterAffiliatedLink h3Text={"Join the MOCKEA Family"} pText={"Give your ideas new life with offers, inspiration and lots of activities in our department stores - completely free of charge"} buttonText={"Become a member here"}/>
+                <FooterAffiliatedLink h3Text={"MOCKEA for business"} pText={"Our company experts help with flexible, affordable and more sustainable solutions."} buttonText={"MOCKEA for business"}/>
             </ul>
             <FooterLinks linksAndText={family} />
             <FooterLinks linksAndText={handla} />
@@ -80,34 +80,21 @@ export const Footer = () => {
         <div className="footer__bottom flex flex-col gap-5">
           <div className="flex justify-between items-center">
             <menu className="footer__links flex gap-6 items-center ">
-              <li><a href=""><i className="fa-brands fa-facebook footer__links-icons"></i></a></li>
-              <li><a href=""><i className="fa-brands fa-instagram footer__links-icons"></i></a></li>
-              <li><a href=""><i className="fa-brands fa-pinterest footer__links-icons"></i></a></li>
-              <li><a href=""><i className="fa-brands fa-youtube footer__links-icons"></i></a></li>
-              <li><a href=""><i className="fa-brands fa-twitter footer__links-icons"></i></a></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/visa.svg" alt="visa" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/master-card.svg" alt="mc" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/american-express.svg" alt="ax" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/ikano.svg" alt="ikano" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/klarna.svg" alt="klarna" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/swish.svg" alt="swish" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/bankid.svg" alt="bankid" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/apple-pay.svg" alt="apple pay" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-secure-logos/visa-secure.svg" alt="visa secure" /></li>
-              <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-secure-logos/master-card-id-check.svg" alt="mc id check" /></li>
+              <FooterSocialsJSX /> 
+              <FooterSupportedPayments />
             </menu>
-            <a href="" className="border border-gray-300 hover:border-black rounded-full flex gap-4 items-center px-5 py-1.5"><i className="fa-solid fa-globe fa-lg "></i>Change country</a>
+           <Link to="/" className="border border-gray-300 hover:border-black rounded-full flex gap-4 items-center px-5 py-1.5"><i className="fa-solid fa-globe fa-lg "></i>Change country</Link>
           </div>
           <hr></hr>
           <div className="text-sm footer__legal flex justify-between mt-4">
-            <p> © Inter IKEA Systems B.V. 1999-2023</p>
+            <p> © Inter MOCKEA Systems B.V. 1999-2023</p>
             <ul className="flex gap-2">
-              <li><a href="" className="hover:underline">Privacy Policy</a></li>
-              <li><a href="" className="hover:underline">Cookie policy</a></li>
-              <li><a href="" className="hover:underline">Responsible Disclosure Policy</a></li>
-              <li><a href="" className="hover:underline">Settings for Cookies</a></li>
-              <li><a href="" className="hover:underline">Purchase & delivery terms</a></li>
-              <li><a href="" className="hover:underline">Company information</a></li>
+              <li><Link to="/" className="hover:underline">Privacy Policy</Link></li>
+              <li><Link to="/" className="hover:underline">Cookie policy</Link></li>
+              <li><Link to="/" className="hover:underline">Responsible Disclosure Policy</Link></li>
+              <li><Link to="/" className="hover:underline">Settings for Cookies</Link></li>
+              <li><Link to="/" className="hover:underline">Purchase & delivery terms</Link></li>
+              <li><Link to="/" className="hover:underline">Company information</Link></li>
             </ul>
           </div>
         </div>
@@ -115,40 +102,3 @@ export const Footer = () => {
     </div>
   )
 }
-
-
-{/* <ul className="footer__handla">
-<li className="text-semibold">Shop at MOCKEA</li>
-<li><a href="">Department store & opening hours</a></li>
-<li><a href="">Payment options</a></li>
-<li><a href="">Delivery options</a></li>
-<li><a href="">Personal planning</a></li>
-<li><a href="">Planning tool</a></li>
-<li><a href="">The entire range</a></li>
-<li><a href="">Product guides</a></li>
-<li><a href="">Brochures</a></li>
-<li><a href="">Gift cards</a></li>
-<li><a href="">The restaurant</a></li>
-</ul>
-<ul className="footer__kundservice">
-<li className="text-semibold">Customer service</li>
-<li><a href="">Customer service</a></li>
-<li><a href="">Contact Us</a></li>
-<li><a href="">Shop by phone</a></li>
-<li><a href="">common questions</a></li>
-<li><a href="">Help us get better</a></li>
-<li><a href="">Service Services</a></li>
-<li><a href="">Returns & complaints</a></li>
-<li><a href="">Purchase & delivery conditions</a></li>
-</ul>
-<ul className="footer__company-info">
-<li className="text-semibold">This is IKEA</li>
-<li><a href="">Work at IKEA</a></li>
-<li><a href="">IKEA Museum</a></li>
-<li><a href="">Life at home</a></li>
-<li><a href="">IKEA Foundation</a></li>
-<li><a href="">Furnishing tomorrow</a></li>
-<li><a href="">Social responsibility</a></li>
-<li><a href="">About us</a></li>
-<li><a href="">Press room</a></li>
-</ul> */}
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Searchbar } from "../searchbar/Searchbar"
 import { SideMenu } from "../SideMenu/SideMenu";
 import { Link } from "react-router-dom";
+import { TopBanner } from "./TopBanner";
 
 export const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState<boolean>(false);
@@ -13,32 +14,29 @@ export const Navbar = () => {
   return (
     <>
       <SideMenu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu}/>
+      <TopBanner />
 
-      <div className="bg-black w-full text-center a-2 text-white top-0"><a href="" className=""><i className="fa-regular fa-message"/> The best gift in the world? Gift card from MOCKEA</a></div>
       <nav className="flex justify-center flex-col max-h-50 pt-2">
         <div className="flex">
 
           <ul className=" container flex justify-between mx-auto gap-4 py-2 max-h-14 items-center relative ">
-            <button className="flex items-center justify-start absolute flex-col -left-20 text-sm" onClick={() => showMenu()}>
-              <i className="fa-solid fa-bars rounded-full hover:bg-hovergrey py-5 px-3 fa-lg active:bg-clickgrey"/>
-              Menu
-            </button>
+            <button className="flex items-center justify-start absolute flex-col -left-20 text-sm" onClick={() => showMenu()}><i className="fa-solid fa-bars rounded-full hover:bg-hovergrey py-5 px-3 fa-lg active:bg-clickgrey"/>Menu</button>
             <li><Link to="/"><img src={'../../dist/mockea.png'} alt="mockeaLogo" className="h-full max-h-14 py-2.5" /></Link></li>
             <li className="flex grow"><Searchbar /></li>
-            <li><i className="fa-regular fa-user fa-lg" /></li>
-            <li><i className="fa-regular fa-heart fa-lg"/></li>
-            <li><i className="fa-solid fa-cart-shopping fa-lg"/></li>
+            <li><Link to="/"><i className="fa-regular fa-user fa-lg" /></Link></li>
+            <li><Link to="/"><i className="fa-regular fa-heart fa-lg"/></Link></li>
+            <li><Link to="/"><i className="fa-solid fa-cart-shopping fa-lg"/></Link></li>
           </ul>
         </div>
         <ul className="flex mx-auto justify-between border-b w-full container py-2">
           <ul className="flex  gap-8 ">
-            <li className="text-sm"><a href="" className="hover:underline  font-bold">Products</a></li>
-            <li className="text-sm"><a href="" className="hover:underline font-bold">All rooms</a></li>
-            <li className="text-sm"><a href="" className="hover:underline font-bold">Offers</a></li>
+            <li className="text-sm"><Link to="/" className="hover:underline  font-bold">Products</Link></li>
+            <li className="text-sm"><Link to="/" className="hover:underline font-bold">All rooms</Link></li>
+            <li className="text-sm"><Link to="/" className="hover:underline font-bold">Offers</Link></li>
           </ul>
           <ul className="flex  gap-8 ">
-            <li className="text-sm"><a href="" className=" font-bold">14145</a></li>
-            <li className="text-sm"><a href="" className=" font-bold">Stockholm Kungens Kurva</a></li>
+            <li className="text-sm"><Link to="/" className=" font-bold">14145</Link></li>
+            <li className="text-sm"><Link to="/" className=" font-bold">Stockholm Kungens Kurva</Link></li>
           </ul>
         </ul>
       </nav>
