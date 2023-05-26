7258b487ba194dd35cfc86791c369d7b6d9666b9
luc siat <lucsiat@gmail.com>
2023-05-26 16:02:49 +0200

    add styling on trendboard

--- frontend/src/pages/Home/TrendBoard.tsx
+++ frontend/src/pages/Home/TrendBoard.tsx
@@ -6,6 +6,8 @@ import { Loader } from "../../components/Loader";
 
 export const TrendBoard = () => {
 
+  // Once clicked does not appear anymore
+
   const { CMSData } = useContext(CMSDataContext) as CMSDataContextType;
 
   useEffect(() => {
@@ -21,12 +23,12 @@ export const TrendBoard = () => {
     <section className="">
       <div className="flex justify-between items-center ">
         <div>
-          <h3 className="text-semibold text-2xl">Goodbye short days. Hello sun rays</h3>
-          <p className="text-sm">Get ready to recline, dine and feel the sushine on your face. it's the perfect time to prepare your garden, <br/> patio or balcon for warmer weather.</p>
+          <h3 className="text-bold text-2xl"><strong>Goodbye short days. Hello sun rays</strong></h3>
+          <p className="text-sm py-3">Get ready to recline, dine and feel the sushine on your face. it's the perfect time to prepare your garden, patio or balcon for warmer <br/> weather.</p>
         </div>
       <button className="rounded-full px-4 py-2 border my-auto hover:border-black">Browse Outdoor</button>
       </div>
-      <div className="trend__image-grid h-screen">
+      <div className="trend__image-grid h-screen grid gap-6">
         {CMSData.trendBoardImages.map((imageUrl, index) => <img src={imageUrl[0]} className={`grid-area-${index + 1} h-full object-cover`} alt=""/>) }
       </div>
     </section>
