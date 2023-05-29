884e6f164c3506cb9f64d4eb41511431ddfe6212
luc siat <lucsiat@gmail.com>
2023-05-29 15:58:18 +0200

    removes dependency to json until solution is found

--- frontend/src/components/Boards/TrendBoard.tsx
+++ frontend/src/components/Boards/TrendBoard.tsx
@@ -4,20 +4,32 @@ import { CMSDataContext } from "../../context/CMSDataProvider";
 import { Loader } from "../Loader";
 
 
+
+const trendBoardImages = [
+  ["https://www.ikea.com/ext/ingkadam/m/42a9ba82e612d85a/original/PH173386-crop001.jpg?f=xl", ""],
+  ["https://www.ikea.com/ext/ingkadam/m/6bfe121c0e7bb92c/original/PH191268.jpg?f=xs", ""],
+  ["https://www.ikea.com/ext/ingkadam/m/6e8da01f11867411/original/PH184065-crop001.jpg?f=xs", ""],
+  ["https://www.ikea.com/ext/ingkadam/m/b58263b9b2a903/original/PH190435.jpg?f=xs", ""],
+  ["https://www.ikea.com/ext/ingkadam/m/66046cc968449809/original/PH189415-crop001.jpg?f=xs", ""]
+]
+
 export const TrendBoard = () => {
 
   // Once clicked does not appear anymore
 
-  const { CMSData } = useContext(CMSDataContext) as CMSDataContextType;
+  // const { CMSData } = useContext(CMSDataContext) as CMSDataContextType;
 
-  useEffect(() => {
-    console.log(CMSData.trendBoardImages);
-  }, [CMSData])
-  if (CMSData.trendBoardImages == null) {
-    return (
-      <Loader />
-    )
-  }
+  // useEffect(() => {
+  //   console.log(CMSData.trendBoardImages);
+  // }, [CMSData])
+  // if (CMSData.trendBoardImages == null) {
+  //   return (
+  //     <Loader />
+  //   )
+  // }
+
+  const trendBoardImagesJSX = trendBoardImages.map((imageUrl, index) => <img key={imageUrl[0]} src={imageUrl[0]} className={`grid-area-${index + 1} h-full object-cover`} alt=""/>);
+
   return (
     <section className="">
       <div className="flex justify-between items-center ">
@@ -28,7 +40,8 @@ export const TrendBoard = () => {
       <button className="rounded-full px-4 py-2 border my-auto hover:border-black">Browse Outdoor</button>
       </div>
       <div className="trend__image-grid h-screen grid gap-6">
-        {CMSData.trendBoardImages.map((imageUrl, index) => <img src={imageUrl[0]} className={`grid-area-${index + 1} h-full object-cover`} alt=""/>) }
+        {/* {CMSData.trendBoardImages.map((imageUrl, index) => <img key={imageUrl[0]} src={imageUrl[0]} className={`grid-area-${index + 1} h-full object-cover`} alt=""/>) } */}
+        {trendBoardImagesJSX}
       </div>
     </section>
   )
--- frontend/src/components/Footer/FooterSupportedPayments.tsx
+++ frontend/src/components/Footer/FooterSupportedPayments.tsx
@@ -1,18 +1,24 @@
 
 
+
+const supportedPayment = [
+  ["visa", "https://www.ikea.com/global/assets/logos/external-payment-providers/visa.svg"],
+  ["mc", "https://www.ikea.com/global/assets/logos/external-payment-providers/master-card.svg"],
+  ["ax", "https://www.ikea.com/global/assets/logos/external-payment-providers/american-express.svg"],
+  ["ikano", "https://www.ikea.com/global/assets/logos/external-payment-providers/ikano.svg"],
+  ["swish", "https://www.ikea.com/global/assets/logos/external-payment-providers/swish.svg"],
+  ["bankid", "https://www.ikea.com/global/assets/logos/external-payment-providers/bankid.svg"],
+  ["apple pay", "https://www.ikea.com/global/assets/logos/external-payment-providers/apple-pay.svg"],
+  ["visa secure", "https://www.ikea.com/global/assets/logos/external-payment-providers/visa-secure.svg"],
+  ["mc id check", "https://www.ikea.com/global/assets/logos/external-secure-logos/master-card-id-check.svg"]
+]
+
+const supportedPaymentJSX = supportedPayment.map(payment =>  <li><img className="footer__links-square-icons"src={payment[1]} alt={payment[0]} /></li>)
+
 export const FooterSupportedPayments = () => {
   return (
     <>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/visa.svg" alt="visa" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/master-card.svg" alt="mc" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/american-express.svg" alt="ax" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/ikano.svg" alt="ikano" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/klarna.svg" alt="klarna" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/swish.svg" alt="swish" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/bankid.svg" alt="bankid" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-payment-providers/apple-pay.svg" alt="apple pay" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-secure-logos/visa-secure.svg" alt="visa secure" /></li>
-      <li><img className="footer__links-square-icons"src="https://www.ikea.com/global/assets/logos/external-secure-logos/master-card-id-check.svg" alt="mc id check" /></li>
+      {supportedPayment}
     </>
   )
 }
