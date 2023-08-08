e1fe4c84b3e55b3a112bbbe36d2b3102ed9d0a3b
Luc Siat <95415989+Luc-Siat@users.noreply.github.com>
2023-08-08 10:05:17 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useContext, useEffect} from 'react'
-// import { Product } from './types';
-// import { getProducts } from './services/mockeaProductsApi';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -9,21 +7,9 @@ import { ToBeDeveloped } from './pages/ToBeDeveloped';
 import { NotFound } from './pages/NotFound';
 import { ProductsContext } from './context/ProductsContext';
 import { ProductsContextType } from './types';
-// import { ProductsContext } from './context/ProductsContext';
-// import { ProductsContextType } from './types';
+
 
 function App() {
-    const {fetchProducts} = useContext(ProductsContext) as ProductsContextType;
-
-  // const getData =  async () => {
-  //   await fetchProducts();
-  // }
-
-  useEffect(() => {
-    // fetchProducts();
-    // getData();
-  }, [])
-  {/* {products.map((product) => <p>{product.name}hi</p>)} */}
 
   return (
     <>
